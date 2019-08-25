8b83c41b7bbeb17c653821c6cec569d325e36f5e
theEvilReaper <steffenwx@gmail.com>
2019-08-25 16:43:56 +0200

    Add ItemFrame listener

--- src/main/java/de/cosmiqglow/lobby/listener/EntityListener.java
+++ src/main/java/de/cosmiqglow/lobby/listener/EntityListener.java
@@ -4,6 +4,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.hanging.HangingBreakByEntityEvent;
 
 public class EntityListener implements Listener {
 
@@ -14,6 +15,11 @@ public class EntityListener implements Listener {
 
     @EventHandler
     public void onCreature(EntitySpawnEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onItemFrame(HangingBreakByEntityEvent event) {
         event.setCancelled(true);
     }
 }
