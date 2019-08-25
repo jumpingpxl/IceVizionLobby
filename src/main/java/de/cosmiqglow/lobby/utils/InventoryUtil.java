fd43c43ccdf98d02e24ee443ad31521b8b5c9f62
theEvilReaper <steffenwx@gmail.com>
2019-08-25 22:07:00 +0200

    Fix wrong inventory slot

--- src/main/java/de/cosmiqglow/lobby/utils/InventoryUtil.java
+++ src/main/java/de/cosmiqglow/lobby/utils/InventoryUtil.java
@@ -48,30 +48,30 @@ public class InventoryUtil {
         inventory.setItem(3, privatMessage == 0 ? new ItemBuilder(Material.LIME_DYE)
                 .setDisplayName(inventory.getItem(3).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
         inventory.setItem(4, privatMessage == 1 ? new ItemBuilder(Material.ORANGE_DYE)
-                .setDisplayName(inventory.getItem(3).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
+                .setDisplayName(inventory.getItem(4).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
         inventory.setItem(5, privatMessage == 2 ? new ItemBuilder(Material.ROSE_RED)
-                .setDisplayName(inventory.getItem(3).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
+                .setDisplayName(inventory.getItem(5).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
 
         inventory.setItem(12, party == 0 ? new ItemBuilder(Material.LIME_DYE)
-                .setDisplayName(inventory.getItem(3).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
+                .setDisplayName(inventory.getItem(12).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
         inventory.setItem(13, party == 1 ? new ItemBuilder(Material.ORANGE_DYE)
-                .setDisplayName(inventory.getItem(3).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
+                .setDisplayName(inventory.getItem(13).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
         inventory.setItem(14, party == 2 ? new ItemBuilder(Material.ROSE_RED)
-                .setDisplayName(inventory.getItem(3).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
+                .setDisplayName(inventory.getItem(14).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
 
         inventory.setItem(21, friend == 0 ? new ItemBuilder(Material.LIME_DYE)
-                .setDisplayName(inventory.getItem(3).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
+                .setDisplayName(inventory.getItem(21).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
         inventory.setItem(22, friend == 1 ? new ItemBuilder(Material.ORANGE_DYE)
-                .setDisplayName(inventory.getItem(3).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
+                .setDisplayName(inventory.getItem(22).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
         inventory.setItem(23, friend == 2 ? new ItemBuilder(Material.ROSE_RED)
-                .setDisplayName(inventory.getItem(3).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
+                .setDisplayName(inventory.getItem(23).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
 
         inventory.setItem(30, jump == 0 ? new ItemBuilder(Material.LIME_DYE)
-                .setDisplayName(inventory.getItem(3).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
+                .setDisplayName(inventory.getItem(30).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
         inventory.setItem(31, jump == 1 ? new ItemBuilder(Material.ORANGE_DYE)
-                .setDisplayName(inventory.getItem(3).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
+                .setDisplayName(inventory.getItem(31).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
         inventory.setItem(32, jump == 2 ? new ItemBuilder(Material.ROSE_RED)
-                .setDisplayName(inventory.getItem(3).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
+                .setDisplayName(inventory.getItem(32).getItemMeta().getDisplayName()).build() : inventory.getItem(3));
         return inventory;
     }
 
