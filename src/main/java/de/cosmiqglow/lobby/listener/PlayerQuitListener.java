4c6c20653059150dd7680153086dbf6ccd732ff1
theEvilReaper <steffenwx@gmail.com>
2019-08-28 20:44:18 +0200

    Add plugin variable to the listener

--- src/main/java/de/cosmiqglow/lobby/listener/PlayerQuitListener.java
+++ src/main/java/de/cosmiqglow/lobby/listener/PlayerQuitListener.java
@@ -1,14 +1,22 @@
 package de.cosmiqglow.lobby.listener;
 
+import de.cosmiqglow.lobby.Lobby;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerQuitListener implements Listener {
 
+    private final Lobby plugin;
+
+    public PlayerQuitListener(Lobby plugin) {
+        this.plugin = plugin;
+    }
+
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         event.setQuitMessage(null);
-    }
 
+        plugin.getInventoryUtil().getPanelCache().remove(event.getPlayer());
+    }
 }
