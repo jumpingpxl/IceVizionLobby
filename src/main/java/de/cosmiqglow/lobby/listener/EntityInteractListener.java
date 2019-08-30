f6737ec6f8dc673241adc481c430b6ed84631b5b
theEvilReaper <steffenwx@gmail.com>
2019-08-30 23:32:03 +0200

    Change event

--- src/main/java/de/cosmiqglow/lobby/listener/EntityInteractListener.java
+++ src/main/java/de/cosmiqglow/lobby/listener/EntityInteractListener.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityInteractEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class EntityInteractListener implements Listener {
@@ -17,15 +18,16 @@ public class EntityInteractListener implements Listener {
     }
 
     @EventHandler
-    public void onArmorInteract(PlayerInteractEntityEvent event) {
+    public void onArmorInteract(PlayerInteractAtEntityEvent event) {
         event.setCancelled(true);
 
-        if (!event.getRightClicked().equals(EntityType.ARMOR_STAND)) return;
+        if (event.getRightClicked().getType().equals(EntityType.ARMOR_STAND)) {
+            ArmorStand armorStand = (ArmorStand) event.getRightClicked();
 
-        ArmorStand armorStand = (ArmorStand) event.getRightClicked();
-
-        if (armorStand.getCustomName().equals("Daily-Reward")) {
-            dailyRewardUtil.giveReward(event.getPlayer());
+            if (armorStand.getCustomName().equalsIgnoreCase("Daily-Reward")) {
+                dailyRewardUtil.giveReward(event.getPlayer());
+            }
         }
+
     }
 }
