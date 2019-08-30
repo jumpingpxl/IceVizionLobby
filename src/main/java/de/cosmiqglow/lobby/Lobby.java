886605325a896e18b6eb00414c712058214da479
theEvilReaper <steffenwx@gmail.com>
2019-08-30 23:20:45 +0200

    Add new listener

--- src/main/java/de/cosmiqglow/lobby/Lobby.java
+++ src/main/java/de/cosmiqglow/lobby/Lobby.java
@@ -45,6 +45,7 @@ public class Lobby extends JavaPlugin {
     }
 
     private void registerListener() {
+        getServer().getPluginManager().registerEvents(new EntityInteractListener(dailyRewardUtil), this);
         getServer().getPluginManager().registerEvents(new EntityListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerBlockListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerDamageListener(), this);
