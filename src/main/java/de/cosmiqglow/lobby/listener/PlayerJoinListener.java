83d64cf6805dfcc36fa83be62fe1879b239baf87
theEvilReaper <steffenwx@gmail.com>
2019-08-31 12:28:58 +0200

    Add first join

--- src/main/java/de/cosmiqglow/lobby/listener/PlayerJoinListener.java
+++ src/main/java/de/cosmiqglow/lobby/listener/PlayerJoinListener.java
@@ -1,6 +1,12 @@
 package de.cosmiqglow.lobby.listener;
 
+import de.cosmiqglow.aves.chat.Messages;
+import de.cosmiqglow.aves.item.FireworkBuilder;
 import de.cosmiqglow.lobby.Lobby;
+import net.titan.spigot.Cloud;
+import net.titan.spigot.player.CloudPlayer;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,5 +27,21 @@ public class PlayerJoinListener implements Listener {
         Player player = event.getPlayer();
 
         plugin.getItemUtil().setItems(player);
+
+        CloudPlayer cloudPlayer = Cloud.getInstance().getPlayer(player);
+
+        if (cloudPlayer.getFirstLogin() + 5000L >= System.currentTimeMillis()) {
+            new FireworkBuilder().addEffect(FireworkEffect.builder().with(FireworkEffect.Type.STAR).
+                    withColor(Color.YELLOW).build()).setPower(1).spawn(player.getLocation());
+            player.sendMessage(Messages.CG_HEADER.toString());
+            player.sendMessage("§e» §n§7Lobby");
+            player.sendMessage(" ");
+            player.sendMessage("§e» §7'Das Glück ist das Einzige,");
+            player.sendMessage( "  §7was sich verdoppelt, wenn es teilt'");
+            player.sendMessage("§e» Albert Schweizer");
+            player.sendMessage(" ");
+            player.sendMessage("§e» §7Map: §eLobby");
+            player.sendMessage(Messages.CG_HEADER.toString());
+        }
     }
 }
