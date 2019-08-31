b6b8f566e74d8db2c526e7efda0d18a01f7a761f
theEvilReaper <steffenwx@gmail.com>
2019-08-31 15:26:25 +0200

    Update constructor

--- src/main/java/de/cosmiqglow/lobby/Lobby.java
+++ src/main/java/de/cosmiqglow/lobby/Lobby.java
@@ -41,7 +41,7 @@ public class Lobby extends JavaPlugin {
         this.cooldownUtil = new CooldownUtil();
         this.itemUtil = new ItemUtil();
         this.settingsUtil = new SettingsUtil();
-        this.inventoryUtil = new InventoryUtil(itemUtil, settingsUtil);
+        this.inventoryUtil = new InventoryUtil(this);
         this.profileCache = new ProfileCache();
         this.visibilityUtil = new VisibilityUtil(profileCache);
         this.dailyRewardUtil = new DailyRewardUtil(mapService.getLocation("daily"));
