77acc5ea62431ded028de69a37b6065ad65f62ef
theEvilReaper <steffenwx@gmail.com>
2019-08-31 14:55:20 +0200

    Remove party system

--- src/main/java/de/cosmiqglow/lobby/utils/VisibilityUtil.java
+++ src/main/java/de/cosmiqglow/lobby/utils/VisibilityUtil.java
@@ -3,8 +3,6 @@ package de.cosmiqglow.lobby.utils;
 import com.google.common.base.Preconditions;
 import de.cosmiqglow.component.friendsystem.spigot.FriendProfile;
 import de.cosmiqglow.component.friendsystem.spigot.FriendSystem;
-import de.cosmiqglow.component.partysystem.spigot.Party;
-import de.cosmiqglow.component.partysystem.spigot.PartySystem;
 import de.cosmiqglow.lobby.profile.LobbyProfile;
 import de.cosmiqglow.lobby.profile.ProfileCache;
 import net.titan.spigot.Cloud;
@@ -37,12 +35,10 @@ public class VisibilityUtil {
             case 1:
                 CloudPlayer cloudPlayer = Cloud.getInstance().getPlayer(player);
                 FriendProfile profile = FriendSystem.getInstance().getFriendProfile(cloudPlayer);
-                Party party = PartySystem.getInstance().getParty(cloudPlayer);
-                if (profile.getFriends().size() == 0 ^ party == null) return;
+                if (profile.getFriends().size() == 0) return;
                 for (Player online : Bukkit.getOnlinePlayers()) {
                     if (online == player) continue;
-                    if (!profile.getRawFriends().containsKey(online.getUniqueId().toString()) ^
-                            !party.getMemberUUIDs().contains(online.getUniqueId().toString())) continue;
+                    if (!profile.getRawFriends().containsKey(online.getUniqueId().toString())) continue;
                     if (!player.canSee(online)) {
                         player.showPlayer(plugin, online);
                     }
@@ -64,20 +60,26 @@ public class VisibilityUtil {
 
     public void hideOnJoin(Plugin plugin, Player joiningPlayer) {
         if (Bukkit.getOnlinePlayers().size() <= 2) return;
-        for (Map.Entry<Player, LobbyProfile> entrySet : profileCache.getProfiles().entrySet()) {
-            Bukkit.broadcastMessage(entrySet.getKey().getDisplayName() + " Setting: " +  entrySet.getValue().getHideSettings());
-            switch (entrySet.getValue().getHideSettings()) {
-                case 2:
-                    entrySet.getKey().hidePlayer(plugin, joiningPlayer);
-                    break;
-                case 1:
-                    CloudPlayer cloudPlayer = Cloud.getInstance().getPlayer(entrySet.getKey());
-                    FriendProfile profile = FriendSystem.getInstance().getFriendProfile(cloudPlayer);
-                    if (!profile.getRawFriends().containsKey(joiningPlayer.getUniqueId().toString())) {
-                        entrySet.getKey().hidePlayer(plugin, joiningPlayer);
+        Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
+            @Override
+            public void run() {
+                for (Map.Entry<Player, LobbyProfile> entrySet : profileCache.getProfiles().entrySet()) {
+                    Bukkit.broadcastMessage(entrySet.getKey().getDisplayName() + " Setting: " +  entrySet.getValue().getHideSettings());
+                    switch (entrySet.getValue().getHideSettings()) {
+                        case 2:
+                            entrySet.getKey().hidePlayer(plugin, joiningPlayer);
+                            break;
+                        case 1:
+                            CloudPlayer cloudPlayer = Cloud.getInstance().getPlayer(entrySet.getKey());
+                            FriendProfile profile = FriendSystem.getInstance().getFriendProfile(cloudPlayer);
+                            if (!profile.getRawFriends().containsKey(joiningPlayer.getUniqueId().toString())) {
+                                entrySet.getKey().hidePlayer(plugin, joiningPlayer);
+                            }
+                            break;
                     }
-                    break;
+                }
             }
-        }
+        }, 10);
+
     }
 }
