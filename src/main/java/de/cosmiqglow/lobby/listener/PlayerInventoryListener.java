d6ca4154ec774949ecf9c7faecd05c41ed7abf74
theEvilReaper <steffenwx@gmail.com>
2019-09-01 20:55:07 +0200

    Fix item move

--- src/main/java/de/cosmiqglow/lobby/listener/PlayerInventoryListener.java
+++ src/main/java/de/cosmiqglow/lobby/listener/PlayerInventoryListener.java
@@ -1,13 +1,13 @@
 package de.cosmiqglow.lobby.listener;
 
 import de.cosmiqglow.lobby.Lobby;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
 
@@ -21,9 +21,6 @@ public class PlayerInventoryListener implements Listener {
 
     @EventHandler
     public void onInventory(InventoryClickEvent event) {
-        event.setCancelled(true);
-        event.setResult(Event.Result.DENY);
-
         if (event.getClickedInventory() == null) return;
         if (event.getSlotType().equals(InventoryType.SlotType.OUTSIDE)) return;
         if (event.getCurrentItem() == null) return;
@@ -31,6 +28,9 @@ public class PlayerInventoryListener implements Listener {
         Player player = (Player) event.getWhoClicked();
 
         ItemStack stack = event.getCurrentItem();
+
+        event.setCancelled(true);
+        event.setResult(Event.Result.DENY);
 
         switch (event.getView().getTitle()) {
             case "Einstellungen":
@@ -46,5 +46,10 @@ public class PlayerInventoryListener implements Listener {
                 player.closeInventory();
                 break;
         }
+    }
+
+    @EventHandler
+    public void onIteMove(InventoryMoveItemEvent event) {
+        event.setCancelled(true);
     }
 }
