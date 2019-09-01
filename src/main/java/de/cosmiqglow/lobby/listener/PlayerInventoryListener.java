63e68dc901696b862c70f6242f44d0cf2846e0d1
theEvilReaper <steffenwx@gmail.com>
2019-09-01 21:02:44 +0200

    Try another fix solution

--- src/main/java/de/cosmiqglow/lobby/listener/PlayerInventoryListener.java
+++ src/main/java/de/cosmiqglow/lobby/listener/PlayerInventoryListener.java
@@ -27,11 +27,6 @@ public class PlayerInventoryListener implements Listener {
         if (event.getCurrentItem() == null) return;
         if (!event.getCurrentItem().hasItemMeta()) return;
 
-        if (event.getClick().equals(ClickType.NUMBER_KEY)) {
-            Bukkit.broadcastMessage("Number-Key");
-            event.setResult(Event.Result.DENY);
-            event.setCancelled(true);
-        }
         Player player = (Player) event.getWhoClicked();
 
         ItemStack stack = event.getCurrentItem();
@@ -41,16 +36,11 @@ public class PlayerInventoryListener implements Listener {
 
         switch (event.getView().getTitle()) {
             case "Einstellungen":
-                if (stack.getItemMeta().getDisplayName().equals("§cSchließen")) {
-                    player.closeInventory();
-                } else {
-                    plugin.getSettingsUtil().changeSettingsValue(event);
-                }
+                plugin.getSettingsUtil().changeSettingsValue(event);
                 break;
             case "Minispiele":
                 String locationName = ChatColor.stripColor(stack.getItemMeta().getDisplayName());
                 player.teleport(plugin.getMapService().getLocation(locationName));
-                player.closeInventory();
                 break;
         }
     }
