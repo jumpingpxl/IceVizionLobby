1a73f4df3c93111c17d81b805135abada73fa590
theEvilReaper <steffenwx@gmail.com>
2019-09-05 18:46:27 +0200

    Rename some variable

--- src/main/java/de/cosmiqglow/lobby/utils/SettingsUtil.java
+++ src/main/java/de/cosmiqglow/lobby/utils/SettingsUtil.java
@@ -21,36 +21,36 @@ public class SettingsUtil {
         CloudPlayer cloudPlayer = Cloud.getInstance().getPlayer((Player) event.getWhoClicked());
         if (event.getCurrentItem().getType().equals(Material.GRAY_DYE)) {
             int currentRow = event.getSlot() / 9;
-            int cat = currentRow * 9;
-            int newValue = (event.getSlot() - cat) - 3;
+            int category = currentRow * 9;
+            int newValue = (event.getSlot() - category) - 3;
             int oldVal = -1;
             for (int i = 0; i < 3; i++) {
                 if (i == newValue) continue;
-                if (!inventory.getItem(cat + ( 3 + i)).getType().equals(Material.GRAY_DYE)) {
+                if (!inventory.getItem(category + ( 3 + i)).getType().equals(Material.GRAY_DYE)) {
                     oldVal = i;
                 }
             }
-            setState(inventory,cat,oldVal, true);
-            setState(inventory,cat,newValue, false);
+            setState(inventory, category, oldVal, true);
+            setState(inventory, category, newValue, false);
             ((Player) event.getWhoClicked()).updateInventory();
             cloudPlayer.setSetting(getSettingsID(currentRow), newValue);
         }
     }
 
-    public void setState(Inventory inv, int kat , int val, boolean gray) {
+    public void setState(Inventory inv, int category , int val, boolean gray) {
         ItemStack state;
         switch (val) {
             case 0:
                 state = new ItemBuilder(gray ? Material.GRAY_DYE : Material.LIME_DYE).setDisplayName("Für alle").build();
-                inv.setItem(kat + ( 3 + val), state);
+                inv.setItem(category + ( 3 + val), state);
                 break;
             case 1:
                 state = new ItemBuilder(gray ? Material.GRAY_DYE : Material.ORANGE_DYE).setDisplayName("Für Freunde").build();
-                inv.setItem(kat + ( 3 + val), state);
+                inv.setItem(category + ( 3 + val), state);
                 break;
             case 2:
                 state = new ItemBuilder(gray ? Material.GRAY_DYE : Material.ROSE_RED).setDisplayName("Für niemanden").build();
-                inv.setItem(kat + ( 3 + val), state);
+                inv.setItem(category + ( 3 + val), state);
                 break;
         }
     }
