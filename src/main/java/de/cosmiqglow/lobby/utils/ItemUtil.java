d7d3bc62d0cc49bdf1ff96d4b6efed1df2578cde
theEvilReaper <steffenwx@gmail.com>
2019-09-15 20:01:06 +0200

    Removed close item and change the position of the request item

--- src/main/java/de/cosmiqglow/lobby/utils/ItemUtil.java
+++ src/main/java/de/cosmiqglow/lobby/utils/ItemUtil.java
@@ -64,14 +64,13 @@ public class ItemUtil {
         for (int i = 36; i < 45; i++) {
             layout.put(i, pane);
         }
-        layout.put(45, new ItemBuilder(Material.EMERALD).setDisplayName("Freundesanfragen").build());
+        layout.put(46, new ItemBuilder(Material.EMERALD).setDisplayName("Freundesanfragen").build());
         layout.put(48, new CustomPlayerHeadBuilder()
                 .setSkinOverValues("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzdhZWU5YTc1YmYwZGY3ODk3MTgzMDE1Y2NhMGIyYTdkNzU1YzYzMzg4ZmYwMTc1MmQ1ZjQ0MTlmYzY0NSJ9fX0=", "" )
                 .setDisplayName("§aZurück").build());
         layout.put(50, new CustomPlayerHeadBuilder()
                 .setSkinOverValues("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNjgyYWQxYjljYjRkZDIxMjU5YzBkNzVhYTMxNWZmMzg5YzNjZWY3NTJiZTM5NDkzMzgxNjRiYWM4NGE5NmUifX19","")
                 .setDisplayName("§aNächste").build());
-        layout.put(53, new ItemBuilder(Material.DARK_OAK_DOOR).setDisplayName("§cSchließen").build());
         return layout;
     }
 
