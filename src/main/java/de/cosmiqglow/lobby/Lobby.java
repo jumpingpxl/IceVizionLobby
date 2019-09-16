2f501949c37a537db3adcf01ea51b55dcaffb9f5
theEvilReaper <steffenwx@gmail.com>
2019-09-16 09:06:23 +0200

    Change constructor

--- src/main/java/de/cosmiqglow/lobby/Lobby.java
+++ src/main/java/de/cosmiqglow/lobby/Lobby.java
@@ -61,7 +61,7 @@ public class Lobby extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new PlayerInventoryListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerItemListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
-        getServer().getPluginManager().registerEvents(new PlayerQuitListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerQuitListener(profileCache), this);
         getServer().getPluginManager().registerEvents(new PlayerSpawnListener(mapService), this);
         getServer().getPluginManager().registerEvents(new WeatherListener(), this);
     }
