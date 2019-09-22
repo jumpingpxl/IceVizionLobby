333b6aff1eaf623cc3084eb539675a48f01da3ed
theEvilReaper <steffenwx@gmail.com>
2019-09-22 19:44:32 +0200

    Remove unused event and import

--- src/main/java/de/icevizion/lobby/listener/PlayerInventoryListener.java
+++ src/main/java/de/icevizion/lobby/listener/PlayerInventoryListener.java
@@ -10,9 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
 
@@ -90,10 +88,5 @@ public class PlayerInventoryListener implements Listener {
                 player.closeInventory();
             }
         }
-    }
-
-    @EventHandler
-    public void onMove(InventoryMoveItemEvent event) {
-        event.setCancelled(true);
     }
 }
