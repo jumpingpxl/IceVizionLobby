3c4abb56bc04b568797cd792015f7f8e148811d2
theEvilReaper <steffenwx@gmail.com>
2019-09-22 13:46:48 +0200

    Add adventure gamemode on join

--- src/main/java/de/icevizion/lobby/listener/PlayerJoinListener.java
+++ src/main/java/de/icevizion/lobby/listener/PlayerJoinListener.java
@@ -1,6 +1,7 @@
 package de.icevizion.lobby.listener;
 
 import de.icevizion.lobby.Lobby;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,6 +20,8 @@ public class PlayerJoinListener implements Listener {
         event.setJoinMessage(null);
 
         Player player = event.getPlayer();
+
+        player.setGameMode(GameMode.ADVENTURE);
 
         plugin.getItemUtil().setItems(player);
         plugin.getProfileCache().addProfile(player);
