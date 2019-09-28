26df6cc3129d87392b2f4fac533082e0f2457add
theEvilReaper <steffenwx@gmail.com>
2019-09-28 13:21:09 +0200

    Add friend feature

--- src/main/java/de/icevizion/lobby/listener/EntityInteractListener.java
+++ src/main/java/de/icevizion/lobby/listener/EntityInteractListener.java
@@ -1,8 +1,14 @@
 package de.icevizion.lobby.listener;
 
+import de.cosmiqglow.component.friendsystem.spigot.FriendProfile;
+import de.cosmiqglow.component.friendsystem.spigot.FriendSystem;
 import de.icevizion.lobby.utils.DailyRewardUtil;
+import net.titan.spigot.Cloud;
+import net.titan.spigot.player.CloudPlayer;
+import org.bukkit.Material;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
@@ -24,6 +30,19 @@ public class EntityInteractListener implements Listener {
 
             if (armorStand.getCustomName().equalsIgnoreCase("§eTägliche Belohnung")) {
                 dailyRewardUtil.checkDailyReward(event.getPlayer());
+            }
+        }
+
+        if (event.getPlayer().getItemOnCursor().equals(Material.PLAYER_HEAD) & event.getRightClicked().getType().equals(EntityType.PLAYER)) {
+            CloudPlayer player = Cloud.getInstance().getPlayer(event.getPlayer());
+            CloudPlayer clickedPlayer = Cloud.getInstance().getPlayer((Player) event.getRightClicked());
+
+            FriendProfile friendProfile = FriendSystem.getInstance().getFriendProfile(player);
+
+            if (friendProfile.getFriends().contains(clickedPlayer)) {
+                return;
+            } else {
+                player.dispatchCommand("friend", new String[]{"add", clickedPlayer.getUsername()});
             }
         }
     }
