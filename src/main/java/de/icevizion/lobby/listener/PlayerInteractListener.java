4fd5465322e193c8218dac387ed9de3b42cdfbe8
theEvilReaper <steffenwx@gmail.com>
2019-10-18 21:30:55 +0200

    Add check for the allowed material

--- src/main/java/de/icevizion/lobby/listener/PlayerInteractListener.java
+++ src/main/java/de/icevizion/lobby/listener/PlayerInteractListener.java
@@ -36,51 +36,55 @@ public class PlayerInteractListener implements Listener {
         if (!event.getItem().hasItemMeta()) return;
         if (!event.getItem().getItemMeta().hasDisplayName()) return;
 
-
-        if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || (event.getAction().equals(Action.RIGHT_CLICK_BLOCK))) {
-            String displayName = event.getItem().getItemMeta().getDisplayName();
-            switch (displayName) {
-                case "§bMinispiele":
-                    player.openInventory(plugin.getInventoryUtil().getTeleporter());
-                    event.setCancelled(true);
-                    player.updateInventory();
-                    break;
-                case "§eEinstellungen":
-                    if (plugin.getProfileCache().getProfile(player).getSettingsInventory() == null) {
-                        Inventory inventory = plugin.getInventoryUtil().createPanelInventory(player);
-                        plugin.getProfileCache().getProfile(player).setSettingsInventory(inventory);
-                        player.openInventory(inventory);
-                    } else {
-                        player.openInventory(plugin.getProfileCache().getProfile(player).getSettingsInventory());
-                    }
-                    break;
-                case "§aSpieler Sichtbarkeit":
-                    if (plugin.getCooldownUtil().hasCooldown(player)) {
-                        player.sendMessage("§cBitte warte noch kurz");
-                    } else {
-                        plugin.getCooldownUtil().addCooldown(player, 4000L);
-                        plugin.getVisibilityUtil().changeVisibility(plugin, player);
-                    }
-                    break;
-                case "§aFreunde":
-                    if (plugin.getProfileCache().getProfile(player).getFriendInventory() == null) {
-                        Inventory inventory = plugin.getInventoryUtil().createFriendInvenotory(player);
-                        plugin.getProfileCache().getProfile(player).setFriendInventory(inventory);
-                        player.openInventory(inventory);
-                    } else {
-                        player.openInventory(plugin.getProfileCache().getProfile(player).getFriendInventory());
-                    }
-                    break;
-                case "§aBauServer":
-                    Optional<ClusterSpigot> clusterSpigot = Cloud.getInstance().getSpigots().stream().filter(server -> server.getServerType().equals("BuildServer")).findAny();
-                    if (clusterSpigot.isPresent()) {
-                        Cloud.getInstance().getPlayer(player).sendToServer(clusterSpigot.get());
-                    } else {
-                        player.sendMessage("§cDer Server ist nicht online");
-                    }
-                default:
-                    break;
+        if (allowMaterial.contains(event.getItem().getType())) {
+            event.setCancelled(false);
+        } else {
+            if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || (event.getAction().equals(Action.RIGHT_CLICK_BLOCK))) {
+                String displayName = event.getItem().getItemMeta().getDisplayName();
+                switch (displayName) {
+                    case "§bMinispiele":
+                        player.openInventory(plugin.getInventoryUtil().getTeleporter());
+                        event.setCancelled(true);
+                        player.updateInventory();
+                        break;
+                    case "§eEinstellungen":
+                        if (plugin.getProfileCache().getProfile(player).getSettingsInventory() == null) {
+                            Inventory inventory = plugin.getInventoryUtil().createPanelInventory(player);
+                            plugin.getProfileCache().getProfile(player).setSettingsInventory(inventory);
+                            player.openInventory(inventory);
+                        } else {
+                            player.openInventory(plugin.getProfileCache().getProfile(player).getSettingsInventory());
+                        }
+                        break;
+                    case "§aSpieler Sichtbarkeit":
+                        if (plugin.getCooldownUtil().hasCooldown(player)) {
+                            player.sendMessage("§cBitte warte noch kurz");
+                        } else {
+                            plugin.getCooldownUtil().addCooldown(player, 4000L);
+                            plugin.getVisibilityUtil().changeVisibility(plugin, player);
+                        }
+                        break;
+                    case "§aFreunde":
+                        if (plugin.getProfileCache().getProfile(player).getFriendInventory() == null) {
+                            Inventory inventory = plugin.getInventoryUtil().createFriendInvenotory(player);
+                            plugin.getProfileCache().getProfile(player).setFriendInventory(inventory);
+                            player.openInventory(inventory);
+                        } else {
+                            player.openInventory(plugin.getProfileCache().getProfile(player).getFriendInventory());
+                        }
+                        break;
+                    case "§aBauServer":
+                        Optional<ClusterSpigot> clusterSpigot = Cloud.getInstance().getSpigots().stream().filter(server -> server.getServerType().equals("BuildServer")).findAny();
+                        if (clusterSpigot.isPresent()) {
+                            Cloud.getInstance().getPlayer(player).sendToServer(clusterSpigot.get());
+                        } else {
+                            player.sendMessage("§cDer Server ist nicht online");
+                        }
+                    default:
+                        break;
+                }
             }
+            event.setCancelled(true);
         }
     }
 }
