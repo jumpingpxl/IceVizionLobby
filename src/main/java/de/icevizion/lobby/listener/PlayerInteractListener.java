e09afff32e706ef8bd6be789e856189b0145f931
theEvilReaper <steffenwx@gmail.com>
2019-11-17 20:58:39 +0100

    Add debug

--- src/main/java/de/icevizion/lobby/listener/PlayerInteractListener.java
+++ src/main/java/de/icevizion/lobby/listener/PlayerInteractListener.java
@@ -2,6 +2,7 @@ package de.icevizion.lobby.listener;
 
 import com.google.common.collect.ImmutableSet;
 import de.icevizion.lobby.Lobby;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
@@ -30,7 +31,9 @@ public class PlayerInteractListener implements Listener {
         if (!event.getItem().getItemMeta().hasDisplayName()) return;
 
         if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || (event.getAction().equals(Action.RIGHT_CLICK_BLOCK))) {
+            Bukkit.broadcastMessage(event.getClickedBlock().getType().name());
             if (allowMaterial.contains(event.getClickedBlock().getType())) {
+
                 event.setCancelled(true);
                 event.setUseInteractedBlock(Event.Result.DENY);
             } else {
