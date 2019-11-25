854bc23070972fcc9922e3d7be5c9621feda16ac
theEvilReaper <steffenwx@gmail.com>
2019-11-25 18:53:45 +0100

    Remove unused code

--- src/main/java/de/icevizion/lobby/listener/PlayerInteractListener.java
+++ src/main/java/de/icevizion/lobby/listener/PlayerInteractListener.java
@@ -1,8 +1,6 @@
 package de.icevizion.lobby.listener;
 
-import com.google.common.collect.ImmutableSet;
 import de.icevizion.lobby.Lobby;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,12 +10,10 @@ import org.bukkit.inventory.Inventory;
 
 public class PlayerInteractListener implements Listener {
 
-    private final ImmutableSet<Material> allowMaterial;
     private final Lobby plugin;
 
     public PlayerInteractListener(Lobby plugin) {
         this.plugin = plugin;
-        this.allowMaterial = ImmutableSet.of(Material.OAK_FENCE, Material.OAK_FENCE_GATE, Material.OAK_TRAPDOOR);
     }
 
     @EventHandler
@@ -27,8 +23,6 @@ public class PlayerInteractListener implements Listener {
         if (event.getItem() == null) return;
         if (!event.getItem().hasItemMeta()) return;
         if (!event.getItem().getItemMeta().hasDisplayName()) return;
-
-        if (event.getAction().equals(Action.PHYSICAL)) event.setCancelled(true);
 
         if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || (event.getAction().equals(Action.RIGHT_CLICK_BLOCK))) {
             String displayName = event.getItem().getItemMeta().getDisplayName();
