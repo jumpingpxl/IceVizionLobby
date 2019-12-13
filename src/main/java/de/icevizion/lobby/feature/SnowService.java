80c876503339d35f195f5934933300b0c30afc70
theEvilReaper <steffenwx@gmail.com>
2019-12-13 20:27:28 +0100

    Add service for christmas snow

---
+++ src/main/java/de/icevizion/lobby/feature/SnowService.java
@@ -0,0 +1,52 @@
+package de.icevizion.lobby.feature;
+
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class SnowService extends BukkitRunnable {
+
+    private static final int RADIUS = 10;
+    private static final int AMOUNT = 25;
+    private final Random random;
+    private static Set<Player> players;
+
+    private final BukkitTask runnable;
+
+    public SnowService(Plugin plugin) {
+        this.players = new HashSet<>();
+        this.random = new Random();
+        this.runnable = this.runTaskTimer(plugin, 0l, 2L);
+    }
+
+    public static void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public static void removePlayer(Player player) {
+        players.remove(player);
+    }
+
+    @Override
+    public void run() {
+        if (players.size() != 0) {
+            for (int i = 0; i < AMOUNT; i++) {
+                float xAdditive = (this.random.nextFloat() - 0.5f) * RADIUS * 2.0f;
+                float max = (float)Math.sqrt(RADIUS * RADIUS - xAdditive * xAdditive) * 2.0f;
+                float yAdditive = (this.random.nextFloat() - 0.5f) * max;
+                float zAdditive = (this.random.nextFloat() - 0.5f) * max;
+
+                for (Player player : players) {
+                    player.spawnParticle(Particle.FIREWORKS_SPARK, player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ(), AMOUNT, xAdditive, yAdditive, zAdditive);
+                }
+            }
+        }
+    }
+}
