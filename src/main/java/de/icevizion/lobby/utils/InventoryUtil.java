feebe9814f9249c54b1edb07376f6ef1f8fd9fb9
theEvilReaper <steffenwx@gmail.com>
2019-12-17 20:32:32 +0100

    Add load of the event category

--- src/main/java/de/icevizion/lobby/utils/InventoryUtil.java
+++ src/main/java/de/icevizion/lobby/utils/InventoryUtil.java
@@ -101,11 +101,13 @@ public class InventoryUtil {
         int party = cloudPlayer.getSetting(SettingsUtil.PARTY);
         int friend = cloudPlayer.getSetting(SettingsUtil.FRIENDS);
         int jump = cloudPlayer.getSetting(SettingsUtil.JUMP);
+        int snow = cloudPlayer.getSetting(SettingsUtil.EVENT);
 
         settingsUtil.setState(inventory, 0, privateMessage,false);
         settingsUtil.setState(inventory, 9, party, false);
         settingsUtil.setState(inventory, 18, friend, false);
         settingsUtil.setState(inventory, 27, jump, false);
+        settingsUtil.setState(inventory, 36, snow, false);
         return inventory;
     }
 
