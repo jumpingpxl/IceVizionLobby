fb60aa270179f2041cf62a442d464e3a4d7b5e5a
theEvilReaper <steffenwx@gmail.com>
2019-12-21 16:24:42 +0100

    [NETWORK-4] Add check for the nametag material

--- src/main/java/de/icevizion/lobby/listener/EntityInteractListener.java
+++ src/main/java/de/icevizion/lobby/listener/EntityInteractListener.java
@@ -1,6 +1,7 @@
 package de.icevizion.lobby.listener;
 
 import de.icevizion.lobby.utils.DailyRewardUtil;
+import org.bukkit.Material;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
@@ -19,11 +20,13 @@ public class EntityInteractListener implements Listener {
     public void onArmorInteract(PlayerInteractAtEntityEvent event) {
         event.setCancelled(true);
 
-        if (event.getRightClicked().getType().equals(EntityType.ARMOR_STAND)) {
-            ArmorStand armorStand = (ArmorStand) event.getRightClicked();
+        if (!event.getPlayer().getItemInHand().getType().equals(Material.NAME_TAG)) {
+            if (event.getRightClicked().getType().equals(EntityType.ARMOR_STAND)) {
+                ArmorStand armorStand = (ArmorStand) event.getRightClicked();
 
-            if (armorStand.getCustomName() != null && armorStand.getCustomName().equals("§eTägliche Belohnung")) {
-                dailyRewardUtil.checkDailyReward(event.getPlayer());
+                if (armorStand.getCustomName() != null && armorStand.getCustomName().equals("§eTägliche Belohnung")) {
+                    dailyRewardUtil.checkDailyReward(event.getPlayer());
+                }
             }
         }
     }
