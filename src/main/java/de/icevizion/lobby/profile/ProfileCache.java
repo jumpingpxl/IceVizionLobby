2b9f359ae167cc2ea771913be4e1bcbc76ab35c2
theEvilReaper <steffenwx@gmail.com>
2019-12-24 12:38:01 +0100

    Add some documentation

--- src/main/java/de/icevizion/lobby/profile/ProfileCache.java
+++ src/main/java/de/icevizion/lobby/profile/ProfileCache.java
@@ -13,17 +13,38 @@ public class ProfileCache {
         this.profiles = new HashMap<>();
     }
 
+    /**
+     * Add a player with a new profile to the service
+     * @param player The player to add
+     */
+
     public void addProfile(Player player) {
         this.profiles.putIfAbsent(player, new LobbyProfile(player));
     }
+
+    /**
+     * Remove a profile from the service
+     * @param player The player to remove
+     */
 
     public void removeProfile(Player player) {
         this.profiles.remove(player);
     }
 
+    /**
+     * Get the profile from a specific player
+     * @param player The player
+     * @return The fetched profile
+     */
+
     public LobbyProfile getProfile(Player player) {
         return this.profiles.get(player);
     }
+
+    /**
+     * Returns the underlying map with all profiles
+     * @return The underlying map
+     */
 
     public Map<Player, LobbyProfile> getProfiles() {
         return profiles;
