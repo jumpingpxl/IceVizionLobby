155c5af9fcd00b291ac6e256fb7c029dc56533ac
theEvilReaper <steffenwx@gmail.com>
2019-12-26 12:18:04 +0100

    Improve constructor

--- src/main/java/de/icevizion/lobby/Lobby.java
+++ src/main/java/de/icevizion/lobby/Lobby.java
@@ -74,7 +74,7 @@ public class Lobby extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new PlayerItemListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerQuitListener(profileCache), this);
-        getServer().getPluginManager().registerEvents(new PlayerSettingsListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerSettingsListener(snowService), this);
         getServer().getPluginManager().registerEvents(new PlayerSpawnListener(mapService), this);
         getServer().getPluginManager().registerEvents(new WeatherListener(), this);
         getServer().getPluginManager().registerEvents(doubleJumpService, this);
