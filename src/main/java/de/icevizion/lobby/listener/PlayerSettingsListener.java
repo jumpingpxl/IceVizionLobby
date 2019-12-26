ae1b605d2db83254afaa6260dd396d7d2b6e8d8d
theEvilReaper <steffenwx@gmail.com>
2019-12-26 12:17:28 +0100

    Add snow check

--- src/main/java/de/icevizion/lobby/listener/PlayerSettingsListener.java
+++ src/main/java/de/icevizion/lobby/listener/PlayerSettingsListener.java
@@ -1,5 +1,6 @@
 package de.icevizion.lobby.listener;
 
+import de.icevizion.lobby.feature.SnowService;
 import de.icevizion.lobby.utils.event.SettingsChangeEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
@@ -7,8 +8,23 @@ import org.bukkit.event.Listener;
 
 public class PlayerSettingsListener implements Listener {
 
+    private final SnowService snowService;
+
+    public PlayerSettingsListener(SnowService snowService) {
+        this.snowService = snowService;
+    }
+
     @EventHandler
     public void onSettings(SettingsChangeEvent event) {
-        Bukkit.broadcastMessage(event.getPlayer().getDisplayName() + " " + event.getCategory());
+        switch (event.getSetting()) {
+            case 199:
+                if (event.getValue() == 1) {
+                    snowService.addPlayer(event.getPlayer());
+                } else {
+                    snowService.removePlayer(event.getPlayer());
+                }
+                break;
+        }
+        Bukkit.broadcastMessage(event.getPlayer().getDisplayName() + " " + event.getSetting());
     }
 }
