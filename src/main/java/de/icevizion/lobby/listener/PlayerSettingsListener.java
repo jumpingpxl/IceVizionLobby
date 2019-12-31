4cf2185c351c198e40c94bd7165bacbe30061140
theEvilReaper <steffenwx@gmail.com>
2019-12-31 12:39:13 +0100

    Remove event check

--- src/main/java/de/icevizion/lobby/listener/PlayerSettingsListener.java
+++ src/main/java/de/icevizion/lobby/listener/PlayerSettingsListener.java
@@ -1,11 +1,7 @@
 package de.icevizion.lobby.listener;
 
 import de.icevizion.lobby.Lobby;
-import de.icevizion.lobby.feature.SnowService;
-import de.icevizion.lobby.utils.SettingsUtil;
 import de.icevizion.lobby.utils.event.SettingsChangeEvent;
-import net.titan.spigot.Cloud;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -20,13 +16,6 @@ public class PlayerSettingsListener implements Listener {
     @EventHandler
     public void onSettings(SettingsChangeEvent event) {
         switch (event.getSetting()) {
-            case SettingsUtil.EVENT:
-                if (event.getValue() == 1) {
-                    plugin.getSnowService().addPlayer(event.getPlayer());
-                } else {
-                    plugin.getSnowService().removePlayer(event.getPlayer());
-                }
-                break;
             /*case SettingsUtil.PLAYER_VISIBILITY:
                 Bukkit.broadcastMessage("Value" + event.getValue());
                 plugin.getVisibilityUtil().changeVisibility(plugin, Cloud.getInstance().getPlayer(event.getPlayer())
