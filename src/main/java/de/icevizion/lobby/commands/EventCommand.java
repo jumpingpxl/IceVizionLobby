68ebf44b362d9b179cdc24a2cd82020396533e35
theEvilReaper <steffenwx@gmail.com>
2019-12-31 12:23:43 +0100

    Add some improvements

--- src/main/java/de/icevizion/lobby/commands/EventCommand.java
+++ src/main/java/de/icevizion/lobby/commands/EventCommand.java
@@ -21,21 +21,20 @@ public class EventCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player) {
-            CloudPlayer cloudPlayer = Cloud.getInstance().getPlayer((Player) sender);
-            Bukkit.broadcastMessage("test");
-            Bukkit.broadcastMessage("" + cloudPlayer.getSetting(SettingsUtil.EVENT));
+            Player player = (Player) sender;
+            CloudPlayer cloudPlayer = Cloud.getInstance().getPlayer(player);
             switch (cloudPlayer.getSetting(SettingsUtil.EVENT)) {
                 case 1:
                     Bukkit.broadcastMessage("test1");
                     cloudPlayer.setSetting(SettingsUtil.EVENT, 0);
-                    plugin.getSnowService().removePlayer(cloudPlayer.getPlayer());
-                    cloudPlayer.sendMessage(plugin.getPrefix() + "§7Du hast den Schnee §cdeaktiviert");
+                    plugin.getSnowService().removePlayer(player);
+                    player.sendMessage(plugin.getPrefix() + "§7Du hast den Schnee §cdeaktiviert");
                     break;
                 case 0:
                     Bukkit.broadcastMessage("test2");
                     cloudPlayer.setSetting(SettingsUtil.EVENT, 1);
-                    plugin.getSnowService().addPlayer(cloudPlayer.getPlayer());
-                    cloudPlayer.sendMessage(plugin.getPrefix() + "§7Du hast den Schnee §aaktiviert");
+                    plugin.getSnowService().addPlayer(player);
+                    player.sendMessage(plugin.getPrefix() + "§7Du hast den Schnee §aaktiviert");
                     break;
             }
         }
