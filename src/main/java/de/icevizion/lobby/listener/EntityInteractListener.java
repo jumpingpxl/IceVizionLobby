3e25a8e78236dda09f62946032d0a2ed0c4c7d47
theEvilReaper <steffenwx@gmail.com>
2020-01-04 15:01:47 +0100

    Add new event

--- src/main/java/de/icevizion/lobby/listener/EntityInteractListener.java
+++ src/main/java/de/icevizion/lobby/listener/EntityInteractListener.java
@@ -3,9 +3,11 @@ package de.icevizion.lobby.listener;
 import de.icevizion.lobby.utils.DailyRewardUtil;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ItemFrame;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class EntityInteractListener implements Listener {
 
@@ -19,13 +21,19 @@ public class EntityInteractListener implements Listener {
 
     @EventHandler
     public void onArmorInteract(PlayerInteractAtEntityEvent event) {
-        event.setCancelled(true);
         if (event.getRightClicked().getType().equals(EntityType.ARMOR_STAND)) {
             ArmorStand armorStand = (ArmorStand) event.getRightClicked();
 
             if (armorStand.getCustomName() != null && armorStand.getCustomName().equals("§eTägliche Belohnung")) {
                 dailyRewardUtil.checkDailyReward(prefix, event.getPlayer());
             }
+        }
+    }
+
+    @EventHandler
+    public void onEntityInteract(PlayerInteractEntityEvent event) {
+        if (event.getRightClicked() instanceof ItemFrame && event.getRightClicked() instanceof ArmorStand) {
+            event.setCancelled(true);
         }
     }
 }
