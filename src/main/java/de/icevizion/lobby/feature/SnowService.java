2ecad01b88ce5da26f35c9e0d12d3d72314b17fe
Rxcki <patrick.zdarsky@gmail.com>
2020-01-05 21:49:23 +0100

    Improve performance of snow particles

--- src/main/java/de/icevizion/lobby/feature/SnowService.java
+++ src/main/java/de/icevizion/lobby/feature/SnowService.java
@@ -1,12 +1,12 @@
 package de.icevizion.lobby.feature;
 
-import net.minecraft.server.v1_13_R2.PacketPlayOutWorldParticles;
-import net.minecraft.server.v1_13_R2.Particles;
-import org.bukkit.craftbukkit.v1_13_R2.entity.CraftPlayer;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,7 +14,10 @@ public final class SnowService extends BukkitRunnable {
 
     private static final Set<Player> players = new HashSet<>();
 
+    private ProtocolManager protocolManager;
+
     public SnowService(Plugin plugin) {
+        protocolManager = ProtocolLibrary.getProtocolManager();
         runTaskTimer(plugin, 0L, 2L);
     }
 
@@ -29,20 +32,24 @@ public final class SnowService extends BukkitRunnable {
     @Override
     public void run() {
         if (players.size() != 0) {
+            WrapperPlayServerWorldParticles packet = new WrapperPlayServerWorldParticles();
+            packet.setOffsetX(10);
+            packet.setOffsetY(10);
+            packet.setOffsetZ(10);
+            packet.setNumberOfParticles(35);
+            packet.setLongDistance(true);
+
             for (Player player : players) {
                 if (player.getWorld().getHighestBlockYAt(player.getEyeLocation()) <= player.getEyeLocation().getY()) {
-                    PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles
-                            (Particles.w,
-                                    true /* LONG DISTANCE*/,
-                                    (float)player.getLocation().getX(),
-                                    (float)player.getLocation().getY() + 10,
-                                    (float)player.getLocation().getZ(),
-                                    10 /* OFFSET X*/,
-                                    10 /* OFFSET Y*/,
-                                    10 /* OFFSET Z*/,
-                                    0 /* data*/,
-                                    35 /* count*/);
-                    ((CraftPlayer)player).getHandle().playerConnection.sendPacket(packet);
+                    packet.setX((float)player.getLocation().getX());
+                    packet.setY((float)player.getLocation().getY() + 10);
+                    packet.setZ((float)player.getLocation().getZ());
+
+                    try {
+                        protocolManager.sendServerPacket(player, packet.getHandle());
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
