f602118c27ebdf3556ded3c03850db2d50c76106
theEvilReaper <steffenwx@gmail.com>
2020-01-10 20:10:41 +0100

    Fix bug that a player can not accept a friend request

--- src/main/java/de/icevizion/lobby/listener/PlayerInventoryListener.java
+++ src/main/java/de/icevizion/lobby/listener/PlayerInventoryListener.java
@@ -160,7 +160,7 @@ public class PlayerInventoryListener implements Listener {
         LobbyProfile profile = plugin.getProfileCache().getProfile(cloudPlayer.getPlayer());
         switch (displayName) {
             case "Annehmen":
-                cloudPlayer.dispatchCommand("friend", new String[]{"accept", name});
+                cloudPlayer.dispatchCommand("friend", new String[]{"accept", ChatColor.stripColor(name)});
                 break;
             case "Ablehnen":
                 cloudPlayer.dispatchCommand("friend", new String[] {"deny", name});
