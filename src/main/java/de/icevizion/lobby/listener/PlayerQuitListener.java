e5474d965a9520f5f6f24d3d28e13dc54e39a2a1
theEvilReaper <steffenwx@gmail.com>
2020-01-18 21:01:55 +0100

    Change to gson constant

--- src/main/java/de/icevizion/lobby/listener/PlayerQuitListener.java
+++ src/main/java/de/icevizion/lobby/listener/PlayerQuitListener.java
@@ -1,12 +1,9 @@
 package de.icevizion.lobby.listener;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import de.icevizion.aves.adapter.LocationTypeAdapter;
+import de.icevizion.lobby.Lobby;
 import de.icevizion.lobby.feature.SnowService;
 import de.icevizion.lobby.profile.ProfileCache;
 import net.titan.spigot.Cloud;
-import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -16,12 +13,9 @@ public class PlayerQuitListener implements Listener {
     private final SnowService snowService;
     private final ProfileCache profileCache;
 
-    private final Gson gson;
-
     public PlayerQuitListener(SnowService snowService, ProfileCache profileCache) {
         this.snowService = snowService;
         this.profileCache = profileCache;
-        this.gson = new GsonBuilder().registerTypeAdapter(Location.class, new LocationTypeAdapter()).create();
     }
 
     @EventHandler
@@ -31,7 +25,7 @@ public class PlayerQuitListener implements Listener {
         snowService.removePlayer(event.getPlayer());
 
         Cloud.getInstance().getPlayer(event.getPlayer()).extradataSet("location",
-                gson.toJson(event.getPlayer().getLocation()));
+                Lobby.GSON.toJson(event.getPlayer().getLocation()));
 
     }
 }
--- src/main/java/de/icevizion/lobby/listener/PlayerSpawnListener.java
+++ src/main/java/de/icevizion/lobby/listener/PlayerSpawnListener.java
@@ -1,9 +1,6 @@
 package de.icevizion.lobby.listener;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import de.icevizion.aves.adapter.LocationTypeAdapter;
+import de.icevizion.lobby.Lobby;
 import de.icevizion.lobby.map.MapService;
 import net.titan.spigot.Cloud;
 import net.titan.spigot.player.CloudPlayer;
@@ -14,19 +11,18 @@ import org.spigotmc.event.player.PlayerSpawnLocationEvent;
 
 public class PlayerSpawnListener implements Listener {
 
-    private final Gson gson;
     private final MapService mapService;
 
     public PlayerSpawnListener(MapService mapService) {
         this.mapService = mapService;
-        this.gson = new GsonBuilder().registerTypeAdapter(Location.class, new LocationTypeAdapter()).create();
     }
 
     @EventHandler
     public void onSpawn(PlayerSpawnLocationEvent event) {
         CloudPlayer cloudPlayer = Cloud.getInstance().getPlayer(event.getPlayer());
         if (cloudPlayer.extradataContains("location")) {
-            event.setSpawnLocation(gson.fromJson((String)cloudPlayer.extradataGet("location"), Location.class));
+            event.setSpawnLocation(
+                    Lobby.GSON.fromJson((String)cloudPlayer.extradataGet("location"), Location.class));
         } else{
             mapService.getLobbyMap().ifPresent(map -> {
                 if (map.getSpawn() != null) {
