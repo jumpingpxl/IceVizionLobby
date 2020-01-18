4ef29151a560a0832772dfca22c4b3b3d633d15a
theEvilReaper <steffenwx@gmail.com>
2020-01-18 20:45:59 +0100

    Add location to extradata

--- src/main/java/de/icevizion/lobby/listener/PlayerQuitListener.java
+++ src/main/java/de/icevizion/lobby/listener/PlayerQuitListener.java
@@ -1,7 +1,12 @@
 package de.icevizion.lobby.listener;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import de.icevizion.aves.adapter.LocationTypeAdapter;
 import de.icevizion.lobby.feature.SnowService;
 import de.icevizion.lobby.profile.ProfileCache;
+import net.titan.spigot.Cloud;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -11,9 +16,12 @@ public class PlayerQuitListener implements Listener {
     private final SnowService snowService;
     private final ProfileCache profileCache;
 
+    private final Gson gson;
+
     public PlayerQuitListener(SnowService snowService, ProfileCache profileCache) {
         this.snowService = snowService;
         this.profileCache = profileCache;
+        this.gson = new GsonBuilder().registerTypeAdapter(Location.class, new LocationTypeAdapter()).create();
     }
 
     @EventHandler
@@ -21,5 +29,9 @@ public class PlayerQuitListener implements Listener {
         event.setQuitMessage(null);
         profileCache.removeProfile(event.getPlayer());
         snowService.removePlayer(event.getPlayer());
+
+        Cloud.getInstance().getPlayer(event.getPlayer()).extradataSet("location",
+                gson.toJson(event.getPlayer().getLocation()));
+
     }
 }
