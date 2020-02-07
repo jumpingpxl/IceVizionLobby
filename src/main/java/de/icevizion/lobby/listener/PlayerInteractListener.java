3717f69ca78ea698241649549215ce438dab5e40
theEvilReaper <steffenwx@gmail.com>
2020-02-07 14:27:02 +0100

    Remove some code

--- src/main/java/de/icevizion/lobby/listener/PlayerInteractListener.java
+++ src/main/java/de/icevizion/lobby/listener/PlayerInteractListener.java
@@ -1,18 +1,12 @@
 package de.icevizion.lobby.listener;
 
 import de.icevizion.lobby.Lobby;
-import net.sf.cglib.asm.$Type;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.util.Vector;
 
 public class PlayerInteractListener implements Listener {
 
@@ -30,16 +24,6 @@ public class PlayerInteractListener implements Listener {
         if (event.getItem() == null) return;
         if (!event.getItem().hasItemMeta()) return;
         if (!event.getItem().getItemMeta().hasDisplayName()) return;
-
-        if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && event.getHand().equals(EquipmentSlot.HAND)) {
-            if (event.getClickedBlock().getType().equals(Material.OAK_STAIRS)
-                    && event.getClickedBlock().getLocation().subtract(0, -1, 0).
-                    getBlock().getType().equals(Material.PURPUR_BLOCK)) {
-                Location arrow = event.getClickedBlock().getLocation().add(0.5, 0, 0.5);
-                Arrow eArrow = (Arrow) player.getWorld().spawnArrow(arrow, new Vector(), 0.0f, 0.0f);
-                eArrow.setPassenger(player);
-            }
-        }
 
         if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || (event.getAction().equals(Action.RIGHT_CLICK_BLOCK))) {
             String displayName = event.getItem().getItemMeta().getDisplayName();
