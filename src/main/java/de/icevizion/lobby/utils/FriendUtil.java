c794de24281762f486f472baf647b5d2a1c25b1b
theEvilReaper <steffenwx@gmail.com>
2020-02-09 22:00:38 +0100

    Improve imports

--- src/main/java/de/icevizion/lobby/utils/FriendUtil.java
+++ src/main/java/de/icevizion/lobby/utils/FriendUtil.java
@@ -1,11 +1,9 @@
 package de.icevizion.lobby.utils;
 
-import de.cosmiqglow.component.friendsystem.spigot.FriendProfile;
 import de.cosmiqglow.component.friendsystem.spigot.FriendSystem;
 import de.icevizion.aves.item.CustomPlayerHeadBuilder;
 import de.icevizion.aves.item.ItemBuilder;
 import de.icevizion.lobby.Lobby;
-import net.titan.spigot.Cloud;
 import net.titan.spigot.player.CloudPlayer;
 import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
@@ -28,15 +26,7 @@ public class FriendUtil {
             inventory.setItem(entry.getKey(), entry.getValue());
         }
 
-        List<CloudPlayer> sortedFriends = FriendSystem.getInstance().getFriendProfile(cloudPlayer).getFriends();
-        sortedFriends.sort((cp1, cp2) -> {
-            if (cp1.isOnline() && cp2.isOnline())
-                return 0;
-            if (cp1.isOnline())
-                return -1;
-            return 1;
-        });
-
+        List<CloudPlayer> sortedFriends = sortPlayers(cloudPlayer);
         for (int i = 0; i < 35 && i < sortedFriends.size(); i++) {
             CloudPlayer player = sortedFriends.get(i);
             if (player.isOnline()) {
@@ -53,39 +43,15 @@ public class FriendUtil {
         cloudPlayer.getPlayer().updateInventory();
     }
 
-    public void updateFriendInventory(CloudPlayer networkPlayer, Map<Integer, ItemStack> layout, Inventory inventory) {
-        inventory.clear();
-        for (Map.Entry<Integer, ItemStack> entry : layout.entrySet()) {
-            inventory.setItem(entry.getKey(), entry.getValue());
+    public List<CloudPlayer> sortPlayers(CloudPlayer cloudPlayer) {
+        List<CloudPlayer> sortedFriends = FriendSystem.getInstance().getFriendProfile(cloudPlayer).getFriends();
+        if (sortedFriends.size() > 1) {
+            sortedFriends.sort((cp1, cp2) -> {
+                int online1 = cp1.isOnline() ? 1 : 0;
+                int online2 = cp2.isOnline() ? 1 : 0;
+                return online2 - online1;
+            });
         }
-
-        for (CloudPlayer player : Cloud.getInstance().getCurrentOnlinePlayers()) {
-            FriendProfile profile = FriendSystem.getInstance().getFriendProfile(player);
-
-            if (profile.getRawFriends().containsKey(networkPlayer.getUuid())) {
-                List<CloudPlayer> sortedFriends = FriendSystem.getInstance().getFriendProfile(player).getFriends();
-                sortedFriends.sort((cp1, cp2) -> {
-                    if (cp1.isOnline() && cp2.isOnline())
-                        return 0;
-                    if (cp1.isOnline())
-                        return -1;
-                    return 1;
-                });
-                for (int i = 0; i < 35 && i < sortedFriends.size(); i++) {
-                    CloudPlayer friend = sortedFriends.get(i);
-                    if (player.isOnline()) {
-                        inventory.setItem(i,new CustomPlayerHeadBuilder()
-                                .setSkinOverValues(friend.getSkinValue(), "")
-                                .addLore("§7Befindet sich auf: §e" + friend.getSpigot().getDisplayName())
-                                .setDisplayName(friend.getFullUsername()).build());
-                    } else {
-                        inventory.setItem(i,new ItemBuilder(Material.SKELETON_SKULL)
-                                .setDisplayName(friend.getFullUsername())
-                                .addLore("§7Zuletzt Online: §e" + Lobby.DATE_FORMAT.format(friend.getLastLogout())).build());
-                    }
-                }
-                player.getPlayer().updateInventory();
-            }
-        }
+        return sortedFriends;
     }
-}
+ }
