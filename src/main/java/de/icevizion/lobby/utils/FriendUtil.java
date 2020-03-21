f532e756d5335bb661fb8024aadf6a1f5de4a8ce
theEvilReaper <steffenwx@gmail.com>
2020-03-21 15:00:45 +0100

    Improve code layout

--- src/main/java/de/icevizion/lobby/utils/FriendUtil.java
+++ src/main/java/de/icevizion/lobby/utils/FriendUtil.java
@@ -36,7 +36,8 @@ public class FriendUtil {
                         .addLore("§7Befindet sich auf: §e" + player.getSpigot().getDisplayName())
                         .setDisplayName(player.getFullUsername()).build());
             } else {
-                inventory.setItem(i, new CustomPlayerHeadBuilder().setSkullType(CustomPlayerHeadBuilder.SkullType.SKELETON)
+                inventory.setItem(i, new CustomPlayerHeadBuilder()
+                        .setSkullType(CustomPlayerHeadBuilder.SkullType.SKELETON)
                         .setDisplayName(player.getFullUsername())
                         .addLore("§7Zuletzt Online: §e" + Lobby.DATE_FORMAT.format(player.getLastLogout())).build());
             }
