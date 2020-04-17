025fcb41a9dbf2778d2ccccc0aff8cf8611d569a
theEvilReaper <steffenwx@gmail.com>
2020-04-17 20:57:00 +0200

    Add event from another class

--- src/main/java/de/icevizion/lobby/listener/EntityListener.java
+++ src/main/java/de/icevizion/lobby/listener/EntityListener.java
@@ -1,11 +1,14 @@
 package de.icevizion.lobby.listener;
 
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.ItemFrame;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.EntityInteractEvent;
 import org.bukkit.event.hanging.HangingBreakByEntityEvent;
 import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class EntityListener implements Listener {
 
@@ -27,5 +30,12 @@ public class EntityListener implements Listener {
     @EventHandler
     public void onItemFrame(HangingBreakByEntityEvent event) {
         event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onEntityInteract(PlayerInteractEntityEvent event) {
+        if (event.getRightClicked() instanceof ItemFrame || event.getRightClicked() instanceof ArmorStand) {
+            event.setCancelled(true);
+        }
     }
 }
