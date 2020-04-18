3592a80fca578c3d1c5a754e20cd948761e648ac
theEvilReaper <steffenwx@gmail.com>
2020-04-18 12:58:50 +0200

    Fix exception

--- src/main/java/de/icevizion/lobby/listener/PlayerSpawnListener.java
+++ src/main/java/de/icevizion/lobby/listener/PlayerSpawnListener.java
@@ -3,12 +3,12 @@ package de.icevizion.lobby.listener;
 import de.icevizion.lobby.map.MapService;
 import net.titan.spigot.Cloud;
 import net.titan.spigot.player.CloudPlayer;
-import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.spigotmc.event.player.PlayerSpawnLocationEvent;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class PlayerSpawnListener implements Listener {
 
@@ -22,10 +22,12 @@ public class PlayerSpawnListener implements Listener {
     public void onSpawn(PlayerSpawnLocationEvent event) {
         CloudPlayer cloudPlayer = Cloud.getInstance().getPlayer(event.getPlayer());
         if (cloudPlayer.extradataContains("location")) {
-            Location location = mapService.getLocationFromMap((HashMap<String, Object>)
-                    cloudPlayer.extradataGet("location"));
-            event.setSpawnLocation(location != null ? location : mapService.getLobbyMap().get().getSpawn());
-
+            if (!(cloudPlayer.extradataGet("location") instanceof Map)) {
+                event.setSpawnLocation(mapService.getLobbyMap().get().getSpawn());
+            } else {
+                event.setSpawnLocation(mapService.getLocationFromMap((HashMap<String, Object>)
+                        cloudPlayer.extradataGet("location")));
+            }
         } else{
             mapService.getLobbyMap().ifPresent(map -> {
                 if (map.getSpawn() != null) {
