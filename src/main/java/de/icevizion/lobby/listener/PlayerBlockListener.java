36defd5e778cf6715b8f273a37501f5ed86b551b
theEvilReaper <steffenwx@gmail.com>
2020-04-22 21:26:17 +0200

    Remove wild card import

--- src/main/java/de/icevizion/lobby/listener/PlayerBlockListener.java
+++ src/main/java/de/icevizion/lobby/listener/PlayerBlockListener.java
@@ -2,7 +2,11 @@ package de.icevizion.lobby.listener;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.*;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockIgniteEvent;
+import org.bukkit.event.block.BlockPhysicsEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.LeavesDecayEvent;
 
 public class PlayerBlockListener implements Listener {
 
