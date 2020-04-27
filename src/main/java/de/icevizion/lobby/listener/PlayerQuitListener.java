b0a4e5d40651beae3ce30b843286ea0a7b034568
theEvilReaper <steffenwx@gmail.com>
2020-04-27 21:30:28 +0200

    Remove profile cache

--- src/main/java/de/icevizion/lobby/listener/PlayerQuitListener.java
+++ src/main/java/de/icevizion/lobby/listener/PlayerQuitListener.java
@@ -1,6 +1,5 @@
 package de.icevizion.lobby.listener;
 
-import de.icevizion.lobby.profile.ProfileCache;
 import net.titan.spigot.Cloud;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,17 +7,9 @@ import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerQuitListener implements Listener {
 
-    private final ProfileCache profileCache;
-
-    public PlayerQuitListener(ProfileCache profileCache) {
-        this.profileCache = profileCache;
-    }
-
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         event.setQuitMessage(null);
-        profileCache.removeProfile(event.getPlayer());
-
         Cloud.getInstance().getPlayer(event.getPlayer()).extradataSet("location",
                 event.getPlayer().getLocation().serialize());
     }
