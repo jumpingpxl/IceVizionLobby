3120edefee89f2c2ab3fde8d445ba52990099cd4
theEvilReaper <steffenwx@gmail.com>
2020-04-28 22:45:01 +0200

    Improve imports

--- src/main/java/de/icevizion/lobby/utils/DoubleJumpService.java
+++ src/main/java/de/icevizion/lobby/utils/DoubleJumpService.java
@@ -13,7 +13,10 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.*;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.util.Vector;
 
 import java.util.HashSet;
